0203c9fdb8477982ea7c9786a23a3aa03cac0518
amo <ieei.ujo.5050@gmail.com>
2021-11-22 11:59:09 +0900

    チャット変更

--- lib/chat.dart
+++ lib/chat.dart
@@ -1,26 +1,240 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_signin_button/flutter_signin_button.dart';
+import 'package:intl/intl.dart';
+
+void main() {
+  runApp(ChatPage());
+}
 
 class ChatPage extends StatelessWidget {
   const ChatPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: '不眠症的な病気'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final _textEditingController = TextEditingController();
+
+  List<String> messages = []; //入力されたメッセージを保存するリスト
+
+  List<String> dates = []; //送信した時間を保存するリスト
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("チャットページ"),
+        leading: Icon(Icons.arrow_back),
+        title: Text(widget.title),
       ),
-      body: Center(
-        // Container(
-        child: SizedBox(
-          width: 200.0,
-          height: 50.0,
-          child: SignInButton(
-            Buttons.Google,
-            text: 'Sign up with Google',
-            onPressed: () {},
+      backgroundColor: Colors.blue[100],
+      body: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: messages.length,
+              itemBuilder: (context, index) {
+                if ((index % 2) == 0) {
+                  //相手か自分を区別
+                  return _ReceivedMessageWidget(
+                    //_ReceivedMessageWidgetに返す
+                    message: messages[index], //messageにmessagesを代入
+                    date: dates[index], //dateにdatesを代入
+                  );
+                } else {
+                  return _SentMessageWidget(
+                    //_SentMessageWidgetに返す
+                    message: messages[index], //dateにdatesを代入
+                    date: dates[index],
+                  );
+                }
+              },
+            ),
           ),
-        ),
+          SingleChildScrollView(
+            child: Container(
+              height: 40,
+              padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: _textEditingController,
+                      cursorColor: Colors.black,
+                      style: const TextStyle(
+                        color: Colors.black,
+                      ),
+                      decoration: InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15),
+                          borderSide: const BorderSide(
+                            color: Colors.black,
+                          ),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15),
+                          borderSide: const BorderSide(
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  IconButton(
+                    icon: const Icon(
+                      Icons.send,
+                      size: 20,
+                    ),
+                    color: Colors.blue,
+                    onPressed: () {
+                      DateFormat _formatter = DateFormat("HH:mm");
+
+                      var msg = _textEditingController.text.trim();
+
+                      var dateadd = _formatter.format(DateTime.now());
+
+                      if (msg.isEmpty) {
+                        return;
+                      }
+                      messages.add(msg);
+
+                      dates.add(dateadd);
+
+                      _textEditingController.clear();
+
+                      setState(() {});
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _ReceivedMessageWidget extends StatelessWidget {
+  final String message;
+
+  final String date;
+  _ReceivedMessageWidget({required this.message, required this.date});
+
+  DateFormat formatter = DateFormat("HH:mm");
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CircleAvatar(),
+          const SizedBox(width: 5),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Column(
+                children: [
+                  const SizedBox(height: 5),
+                  Container(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 5,
+                      horizontal: 10,
+                    ),
+                    constraints: BoxConstraints(
+                      maxWidth: MediaQuery.of(context).size.width * 0.6,
+                    ),
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                    ),
+                    child: Text(
+                      message,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(width: 5),
+              Text(
+                date,
+                style: const TextStyle(fontSize: 10),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _SentMessageWidget extends StatelessWidget {
+  final String message;
+
+  final String date;
+  _SentMessageWidget({required this.message, required this.date});
+
+  final _formatter = DateFormat("HH:mm");
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Text(
+            date,
+            style: const TextStyle(fontSize: 10),
+          ),
+          const SizedBox(width: 5),
+          Column(
+            children: [
+              const SizedBox(height: 5),
+              Container(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 5,
+                  horizontal: 10,
+                ),
+                constraints: BoxConstraints(
+                  maxWidth: MediaQuery.of(context).size.width * 0.6,
+                ),
+                decoration: const BoxDecoration(
+                  color: Colors.lightGreenAccent,
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                ),
+                child: Text(
+                  message,
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
